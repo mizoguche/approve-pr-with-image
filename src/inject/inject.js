df9524e87088db1a149417656e7784754bc36b72
Michiaki Mizoguchi <michiaki.mizoguchi@gmail.com>
2017-01-29 20:27:30 +0900

    Fix jQuery expression

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,11 +1,11 @@
-import $ from 'jquery';
+import jQuery from 'jquery';
 
 const pullCommentQuery = "textarea[name='comment[body]']";
 const pullReviewSummaryQuery = "textarea[name='pull_request_review[body]']";
 
 chrome.runtime.onMessage.addListener((link) => {
-  const pullComment = `${$(pullCommentQuery).val()}\n${link}`;
-  $(pullCommentQuery).val(pullComment);
-  $(pullReviewSummaryQuery).val(pullComment);
-  $("input[name='pull_request_review[event]'][value='approve']").prop('checked', true);
+  const pullComment = `${jQuery(pullCommentQuery).val()}\n${link}`;
+  jQuery(pullCommentQuery).val(pullComment);
+  jQuery(pullReviewSummaryQuery).val(pullComment);
+  jQuery("input[name='pull_request_review[event]'][value='approve']").prop('checked', true);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,33 @@
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    context: __dirname + '/src',
-    entry: {
-        'background': './bg/background.js',
-        'browser_action': './browser_action/browser_action.js',
-        'inject': './inject/inject.js',
-        'options': './options/index.js'
-    },
-    output: {
-        path: __dirname + '/dist',
-        filename: '[name].js'
-    },
-    plugins: [
-        new CopyWebpackPlugin([{from: 'static'}]),
-        new CopyWebpackPlugin([{from: '../node_modules/material-design-lite/material.min.css'}]),
-        new CopyWebpackPlugin([{from: '../node_modules/material-design-lite/material.min.js'}]),
-        new CopyWebpackPlugin([{from: '../node_modules/jquery/dist/jquery.min.js'}])
-    ],
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: "babel",
-                query: {
-                    presets: ['es2015'],
-                    "plugins": ["transform-flow-strip-types"]
-                }
-            }
-        ]
-    }
+  context: __dirname + '/src',
+  entry: {
+    'background': './bg/background.js',
+    'browser_action': './browser_action/browser_action.js',
+    'inject': './inject/inject.js',
+    'options': './options/index.js'
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].js'
+  },
+  plugins: [
+    new CopyWebpackPlugin([{ from: 'static' }]),
+    new CopyWebpackPlugin([{ from: '../node_modules/material-design-lite/material.min.css' }]),
+    new CopyWebpackPlugin([{ from: '../node_modules/material-design-lite/material.min.js' }]),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel",
+        query: {
+          presets: ['es2015'],
+          "plugins": ["transform-flow-strip-types"]
+        }
+      }
+    ]
+  }
 };
