f38ac286e77e51195ce70236c04c7278ed2773e4
Michiaki Mizoguchi <michiaki.mizoguchi@gmail.com>
2017-02-19 14:25:27 +0900

    Add slack notification

---
+++ src/domain/ChromeStorage.js
@@ -0,0 +1,27 @@
+import { Images } from './image';
+
+export default class ChromeStorage {
+  constructor() {
+    this.storage = chrome.storage.sync;
+  }
+
+  fetch(callback) {
+    this.storage.get('images', (obj) => {
+      if (!obj.images) {
+        callback(new Images());
+        return;
+      }
+      const images = new Images();
+      images.images = obj.images;
+      callback(images);
+    });
+  }
+
+  store(images: Images, callback: Function) {
+    this.storage.set({ images: images.images }, () => {
+      if (callback) {
+        callback();
+      }
+    });
+  }
+}
