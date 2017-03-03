cb6c3bf11e3501c68f52388b489403aeda5194db
Michiaki Mizoguchi <michiaki.mizoguchi@gmail.com>
2017-03-04 00:59:20 +0900

    Remove binding in jsx

--- src/components/RawUrlOption.jsx
+++ src/components/RawUrlOption.jsx
@@ -6,12 +6,6 @@ import type { State } from '../reducers/option';
 import { updateRawUrls, editRawUrls } from '../actions/option';
 
 class RawUrlOption extends Component {
-  constructor(props, context, updater) {
-    super(props, context, updater);
-    (this: any).handleSave = this.handleSave.bind(this);
-    (this: any).handleEdit = this.handleEdit.bind(this);
-  }
-
   props: {
     rawUrls: string,
     updateRawUrls: Function,
@@ -20,28 +14,23 @@ class RawUrlOption extends Component {
 
   textarea: HTMLTextAreaElement;
 
-  handleSave() {
-    this.props.updateRawUrls(this.textarea.value);
-  }
-
-  handleEdit(e) {
-    this.props.editRawUrls(e.target.value);
-  }
-
   render() {
     return (
       <form>
         <div className="form-group">
           <label htmlFor="raw0url-textarea">Raw URL List(Separate with line break)</label>
           <textarea
-            id="raw-url-textarea" className="form-control" rows="20" value={this.props.rawUrls}
-            ref={(textarea) => { this.textarea = textarea; }} onChange={this.handleEdit}
+            id="raw-url-textarea"
+            className="form-control"
+            rows="20" value={this.props.rawUrls}
+            ref={(textarea) => { this.textarea = textarea; }}
+            onChange={e => this.props.editRawUrls(e.target.value)}
           />
         </div>
         <button
           id="save"
           className="btn btn-primary"
-          onClick={this.handleSave}
+          onClick={() => this.props.updateRawUrls(this.textarea.value)}
         >
           Save
         </button>
