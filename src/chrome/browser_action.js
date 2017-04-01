8e3edfdb076087dbb32a4038ccae31a8c23dccd0
Michiaki Mizoguchi <michiaki.mizoguchi@gmail.com>
2017-04-01 22:47:36 +0900

    Fix browser action

--- src/chrome/browser_action.js
+++ src/chrome/browser_action.js
@@ -1,39 +1,34 @@
 // @flow
 import jQuery from 'jquery';
+import Clipboad from 'clipboard';
+
 import { imageRepository } from '../application/repositories';
 
-const copy = () => {
-  jQuery('#clipboard').show();
-  jQuery('#clipboard').focus();
-  jQuery('#clipboard').select();
-  document.execCommand('Copy');
-  window.getSelection().removeAllRanges();
-  jQuery('#clipboard').hide();
-};
-
-// TODO: Use callback that MDL snackbar is ready
 jQuery(document).ready(() => {
-  setTimeout(() => {
-    imageRepository.fetch().subscribe((images) => {
-      if (images.isEmpty()) {
-        chrome.runtime.openOptionsPage();
-        return;
-      }
+  const clipboard = new Clipboad('.btn');
+  clipboard.on('success', (e) => {
+    console.info(`Copied to clipboard: ${e.text}`)
+  });
 
-      const img = images.getRandom();
-      const imageUrl = img.src;
-      jQuery('main').empty().append(`<img style="min-width: 100px; min-height: 100px;max-width: 500px; max-height: 500px;" src="${imageUrl}">`);
-      const copyString = `[![LGTM](${imageUrl})](${imageUrl})`;
-      jQuery('#clipboard').text(copyString);
-      copy();
-      const snackbar: any = document.querySelector('#copy-message');
-      snackbar.MaterialSnackbar.showSnackbar({ message: 'Copied to clipboard.' });
+  imageRepository.fetch().subscribe((images) => {
+    if (images.isEmpty()) {
+      chrome.runtime.openOptionsPage();
+      return;
+    }
 
-      chrome.tabs.query({ active: true }, (tab: any) => {
-        const tabId: number = tab[0].id;
-        chrome.tabs.sendMessage(tabId, copyString, () => {
-        });
+    const img = images.getRandom();
+    const imageUrl = img.src;
+    jQuery('main').empty().append(`<img style="min-width: 100px; min-height: 100px;max-width: 500px; max-height: 500px;" src="${imageUrl}">`);
+    const copyString = `[![LGTM](${imageUrl})](${imageUrl})`;
+    jQuery('#clipboard').attr('data-clipboard-text', copyString);
+    jQuery('#clipboard').click();
+    jQuery('#alert').show();
+
+
+    chrome.tabs.query({ active: true }, (tab: any) => {
+      const tabId: number = tab[0].id;
+      chrome.tabs.sendMessage(tabId, copyString, () => {
       });
     });
-  }, 10);
+  });
 });
