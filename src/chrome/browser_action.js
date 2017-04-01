031044b878116249d14bb0fa773df332d491bb88
Michiaki Mizoguchi <michiaki.mizoguchi@gmail.com>
2017-04-01 22:53:20 +0900

    Refactor browser action

--- src/chrome/browser_action.js
+++ src/chrome/browser_action.js
@@ -4,31 +4,46 @@ import Clipboad from 'clipboard';
 
 import { imageRepository } from '../application/repositories';
 
+const createImageElement = (imageUrl: string) => {
+  jQuery('main')
+    .empty()
+    .append(`<img style="min-width: 100px; min-height: 100px;max-width: 500px; max-height: 500px;" src="${imageUrl}">`);
+};
+
+const copyToClipboard = (text: string) => {
+  jQuery('#clipboard').attr('data-clipboard-text', text);
+  jQuery('#clipboard').click();
+  jQuery('#alert').show();
+};
+
+const notifyTextToTab = (text: string) => {
+  chrome.tabs.query({ active: true }, (tab: any) => {
+    const tabId: number = tab[0].id;
+    chrome.tabs.sendMessage(tabId, text, () => {
+    });
+  });
+};
+
 jQuery(document).ready(() => {
   const clipboard = new Clipboad('.btn');
   clipboard.on('success', (e) => {
     console.info(`Copied to clipboard: ${e.text}`)
   });
 
-  imageRepository.fetch().subscribe((images) => {
-    if (images.isEmpty()) {
-      chrome.runtime.openOptionsPage();
-      return;
-    }
+  imageRepository
+    .fetch()
+    .subscribe((images) => {
+      if (images.isEmpty()) {
+        chrome.runtime.openOptionsPage();
+        return;
+      }
 
-    const img = images.getRandom();
-    const imageUrl = img.src;
-    jQuery('main').empty().append(`<img style="min-width: 100px; min-height: 100px;max-width: 500px; max-height: 500px;" src="${imageUrl}">`);
-    const copyString = `[![LGTM](${imageUrl})](${imageUrl})`;
-    jQuery('#clipboard').attr('data-clipboard-text', copyString);
-    jQuery('#clipboard').click();
-    jQuery('#alert').show();
+      const img = images.getRandom();
+      const imageUrl = img.src;
+      createImageElement(imageUrl);
 
-
-    chrome.tabs.query({ active: true }, (tab: any) => {
-      const tabId: number = tab[0].id;
-      chrome.tabs.sendMessage(tabId, copyString, () => {
-      });
-    });
+      const imageText = `[![LGTM](${imageUrl})](${imageUrl})`;
+      copyToClipboard(imageText);
+      notifyTextToTab(imageText);
   });
 });
