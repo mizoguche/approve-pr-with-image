f88d262bff395589b6168a434dac0a4012da2fa1
Michiaki Mizoguchi <michiaki.mizoguchi@gmail.com>
2017-04-08 21:45:33 +0900

    Use object spread operator

--- src/reducers/option.js
+++ src/reducers/option.js
@@ -2,53 +2,66 @@
 import Images from '../domain/Images';
 import * as types from '../actions/types';
 
-export class State {
-  option: OptionState;
-  routing: any;
-}
-
-export class OptionState {
+export type OptionState = {
   images: Images;
   rawUrls: string;
   previewingImageSrc: string;
   isPreviewing: boolean;
-
-  constructor(rawUrls: string, previewingImageSrc: string = '', isPreviewing: boolean = false) {
-    this.images = new Images(rawUrls);
-    this.rawUrls = rawUrls;
-    this.previewingImageSrc = previewingImageSrc;
-    this.isPreviewing = isPreviewing;
-  }
 }
+
+export type State = {
+  option: OptionState;
+  routing: any;
+}
+
+const defaultOptionState: OptionState = {
+  images: new Images(''),
+  rawUrls: '',
+  previewingImageSrc: '',
+  isPreviewing: false,
+};
 
 const buildRawUrls = images => images.images.reduce((a, b) => `${a}${b.src}\n`, '');
 
-export default (state: OptionState = new OptionState(''), action: any): OptionState => {
+export default (
+  state: OptionState = defaultOptionState,
+  action: any,
+): OptionState => {
   switch (action.type) {
     case types.COMPLETE_FETCH_IMAGES:
     case types.COMPLETE_UPDATE_RAW_URLS:
-    case types.COMPLETE_REMOVE_IMAGE:
-      {
-        const images = (action: types.Action<types.ImagesPayload>).payload.images;
-        const rawUrls = buildRawUrls(images);
-        return new OptionState(rawUrls);
-      }
+    case types.COMPLETE_REMOVE_IMAGE: {
+      const images = (action: types.Action<types.ImagesPayload>).payload.images;
+      const rawUrls = buildRawUrls(images);
+      return {
+        ...state,
+        images,
+        rawUrls,
+      };
+    }
 
-    case types.EDIT_RAW_URLS:
-      {
-        const rawUrls = (action: types.Action<types.RawUrlsPayload>).payload.rawUrls;
-        return new OptionState(rawUrls);
-      }
+    case types.EDIT_RAW_URLS: {
+      const rawUrls = (action: types.Action<types.RawUrlsPayload>).payload.rawUrls;
+      return {
+        ...state,
+        images: new Images(rawUrls),
+        rawUrls,
+      };
+    }
 
     case types.SHOW_PREVIEW:
-      return new OptionState(
-        state.rawUrls,
-        (action: types.Action<types.SrcPayload>).payload.src,
-        true,
-      );
+      return {
+        ...state,
+        previewingImageSrc: (action: types.Action<types.SrcPayload>).payload.src,
+        isPreviewing: true,
+      };
 
     case types.HIDE_PREVIEW:
-      return new OptionState(state.rawUrls, '', false);
+      return {
+        ...state,
+        previewingImageSrc: '',
+        isPreviewing: false,
+      };
 
     default:
       return state;
