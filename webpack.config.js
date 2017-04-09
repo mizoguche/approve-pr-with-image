de0e1d1354b62aea0518f5273d68013eb58c5491
Michiaki Mizoguchi <michiaki.mizoguchi@gmail.com>
2017-04-09 16:02:20 +0900

    Integrate css settings and minimize

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,10 @@ const plugins = [
     Tether: 'tether',
   }),
   new webpack.DefinePlugin({ ENV: JSON.stringify(process.env.NODE_ENV) }),
+  extractSass,
+  new CopyWebpackPlugin([
+    { from: 'chrome/static/html' },
+  ]),
 ];
 
 if (process.env.NODE_ENV === 'production') {
@@ -25,7 +29,6 @@ if (process.env.NODE_ENV === 'production') {
 
 
 module.exports = [
-  // JS
   {
     context: `${__dirname}/src`,
     entry: {
@@ -33,6 +36,7 @@ module.exports = [
       browser_action: './chrome/browser_action',
       inject: './chrome/inject',
       options: './option',
+      style: './chrome/static/style/index.js',
     },
     output: {
       path: `${__dirname}/dist`,
@@ -50,54 +54,35 @@ module.exports = [
             plugins: ['transform-flow-strip-types'],
           },
         },
-      ],
-    },
-    resolve: {
-      extensions: ['.js', '.jsx'],
-    },
-    devtool: sourceMap,
-  },
-
-
-  // SCSS
-  {
-    context: `${__dirname}/src`,
-    entry: {
-      style: './chrome/static/style/index.js',
-    },
-    output: {
-      path: `${__dirname}/dist`,
-      filename: '[name].js',
-    },
-    module: {
-      loaders: [
         {
           test: /\.scss$/,
-          loader: ExtractTextPlugin.extract('css-loader!sass-loader'),
+          loader: ExtractTextPlugin.extract('css-loader?minimize!sass-loader'),
         },
         {
           test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
           loader: 'url-loader?limit=10000&mimetype=application/font-woff',
-        }, {
+        },
+        {
           test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
           loader: 'url-loader?limit=10000&mimetype=application/font-woff',
-        }, {
+        },
+        {
           test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
           loader: 'url-loader?limit=10000&mimetype=application/octet-stream',
-        }, {
+        },
+        {
           test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
           loader: 'file-loader',
-        }, {
+        },
+        {
           test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
           loader: 'url-loader?limit=10000&mimetype=image/svg+xml',
-        }
+        },
       ],
     },
-    plugins: [
-      extractSass,
-      new CopyWebpackPlugin([
-        { from: 'chrome/static/html' },
-      ]),
-    ],
+    resolve: {
+      extensions: ['.js', '.jsx', '.scss', '.css'],
+    },
+    devtool: sourceMap,
   },
 ];
